ee4ad41f3ba76984dc4341b067b156ccba8b9a73
Pablo <camara.gapab23@triana.salesianos.edu>
2025-02-18 13:56:27 +0100

    Agregados los endopints "/error" a la seguridad.

--- ejemploCompletoSeguridad/src/main/java/com/salesianostriana/ejemploCompletoSeguridad/security/SecurityConfig.java
+++ ejemploCompletoSeguridad/src/main/java/com/salesianostriana/ejemploCompletoSeguridad/security/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
         );
 
         http.authorizeHttpRequests(authz -> authz
-                .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login", "/auth/refresh/token").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login", "/auth/refresh/token", "/error").permitAll()
                 .requestMatchers("/me/admin").hasRole("ADMIN")
                 .requestMatchers("h2-console/**").permitAll()
                 .anyRequest().authenticated()
